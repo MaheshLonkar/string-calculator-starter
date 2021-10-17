2d6823f57237f1ca29d5ba6d7da60937162462eb
Mahesh Lonkar <maheshlonkar248@gmail.com>
2021-10-17 19:24:36 +0530

    test case 4 is writtem to support diffrent delimiters

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -39,4 +39,8 @@ class StringCalculatorShould {
     public void testNewLine(){
     	assertEquals(6, Calculator.add("1\n2,3"));
     }
+    @Test
+    public void testOtherDelimiter(){
+    	assertEquals(3, Calculator.add("//;\n1;2"));
+    }
 }
